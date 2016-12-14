701c7b5a3d325ee4f2bec004c5b2ca7e6b0da9dd
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2016-12-13 19:22:36 -0700

    Code written.

--- Gemfile
+++ Gemfile
@@ -29,12 +29,17 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'bootstrap_form'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
   gem 'rspec-rails'
   gem 'capybara'
   gem 'launchy'
+  gem "factory_girl_rails"
+  gem "database_cleaner"
+  gem 'shoulda-matchers'
 end
 
 group :development do
---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,31 @@
+class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to artist_path(@artist)
+    else
+      @errors = @artist.errors
+      render :new
+    end
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :image_path)
+  end
+  
+end
